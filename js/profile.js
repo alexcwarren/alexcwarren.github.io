f650d709fef5197f10c1eb47625075e4f5afce6c
Alex Warren <alexander.ch.warren@gmail.com>
2020-05-11 07:45:39 -0400

    Move submit() to profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,16 @@
+function submit() {
+  var userId = firebase.auth().currentUser.uid;
+
+  var data = {
+    username: document.getElementById('username').value,
+    score: document.getElementById('score').value
+  };
+  console.log(data);
+
+  var dataRef = database.ref('users/' + userId + '/data');
+  dataRef.set(data);
+}
+
 window.onload = function() {
   initApp();
 
