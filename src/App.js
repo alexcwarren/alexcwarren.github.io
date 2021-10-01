468026fb7842eadbf2fcd387880e47590cb173db
Alex Warren <alexander.ch.warren@gmail.com>
2021-10-01 11:56:34 -0400

    Add profile photo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import profile from './profilePhoto.JPG';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   document.title = "alexcwarren.com";
-  
+
   return (
     <div className="App">
       <header className="App-header">
+        <img src={profile} className="Profile" alt="profile" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
