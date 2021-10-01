19061ad6f8fba289cb6c4aad135ce1842e42421f
Alex Warren <alexander.ch.warren@gmail.com>
2021-10-01 11:09:58 -0400

    Add my name

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  document.title = "alexcwarren.com";
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +11,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          , Alex Warren.
+        </span>
       </header>
     </div>
   );
