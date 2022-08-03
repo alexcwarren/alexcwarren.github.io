856c69d465b79c8611355da9d84b6961fe13f4cb
Alex Warren <alexander.ch.warren@gmail.com>
2022-08-03 11:00:29 -0400

    Update scss-tokenizer & nth-check

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,4 +1,6 @@
 export const buymeacoffee = "https://www.buymeacoffee.com/alexcwarren";
+export const contactme =
+  "https://us12.list-manage.com/contact-form?u=a0893bd472ec99083ffec4e5e&form_id=7b741951a1482747b1d37900ebd554f3";
 export const facebook = "https://www.facebook.com/alex.ch.warren/";
 export const github = "https://github.com/alexcwarren";
 export const instagram = "https://instagram.com/alexcwarren";
